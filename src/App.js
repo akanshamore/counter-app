59832600e9fa9875411db0e1e03f8ba77ec01f68
Akansha More <akanshamore21@gmail.com>
2022-09-18 12:31:02 +0530

    Update App.js
    Created buttons and initial counter state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 
 //import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  
+const App = () => {
+
+  const [count, setCount] = useState(0);
+  //Add Function
+  const add = () => {
+    console.log("click add")
+  }
+  //Subtract Function
+  const subtract = () => {
+    console.log("click subtract")
+  }
+
+const reset=()=> {
+
+console.log("click reset")
+
+
+}
+
+
+
+
 
   return (
     <div >
-    <h1>COUNTER APP</h1>
+      <h1>COUNTER APP</h1>
+      <h1>Count:0</h1>
+      <button onClick={add}>Add</button>
+      <button onClick={subtract}>Subtract</button>
+      <button onClick = {reset}>Reset</button>
     </div>
   );
 }
+
+
 
 export default App;
